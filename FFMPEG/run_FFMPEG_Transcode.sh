9854f294d82ae4725f0abca65e55a699b191aa9e
Edward Shi <shihuade@users.noreply.github.com>
2017-07-13 17:43:06 +0800

    FFMPEG transcode script ready

--- FFMPEG/run_FFMPEG_Transcode.sh
+++ FFMPEG/run_FFMPEG_Transcode.sh
@@ -1,190 +1,141 @@
 #!/bin/bash
+#***************************************************************
+# brief:
+#       transcode mp4
+#       and generate transcode statistic report
+#***************************************************************
 
 
-runUsage
+runUsage()
+{
+    echo -e "\033[31m ***************************************** \033[0m"
+    echo " Usage:                                                "
+    echo "      $0  \$InputDir  \$x264Params  \$Pattern          "
+    echo "                                                       "
+    echo "      --InputDir:   mp4 dir which will be transcoded   "
+    echo "                                                       "
+    echo "      --x264Params: x264 encode parameters             "
+    echo "                                                       "
+    echo "      --Pattern: transcoded file name's postfix        "
+    echo "                                                       "
+    echo -e "\033[31m ***************************************** \033[0m"
+}
 
 runInit()
 {
     TranscodePattern="ffmpeg_trans"
 
+    MP4ParserScript="../MP4Info/run_ParseMP4Info.sh"
+    AllMP4Info="Report_AllMP4Info.csv"
+    AllMP4InfoParserConsole="Report_AllMP4InfoDetail.txt"
+    TranscodeSummaryInfo="Report_TranscodeSummary.csv"
+
+    HeadLine="MP4File, OriginSize(MBs), TranscodedSize(MBs), Delta(%)"
+
+    echo "${HeadLine}">${TranscodeSummaryInfo}
 }
 
-runParseOriginFile()
+runUpdateTranscodeStatic()
 {
+    #parse origin and transcoded mp4 files' info
+    MP4FileName=`basename $Mp4File`
 
-Track 1:
-flags:        1 ENABLED
-id:           1
-type:         Audio
-duration: 13833 ms
-language: und
-media:
-sample count: 598
-timescale:    44100
-duration:     612352 (media timescale units)
-duration:     13886 (ms)
-bitrate (computed): 83.506 Kbps
-Sample Description 0
-Coding:      mp4a (MPEG-4 Audio)
-Stream Type: Audio
-Object Type: MPEG-4 Audio
-Max Bitrate: 128000
-Avg Bitrate: 128000
-Buffer Size: 6144
-Codecs String: mp4a.40.2
-MPEG-4 Audio Object Type: 2 (AAC Low Complexity)
-MPEG-4 Audio Decoder Config:
-Sampling Frequency: 44100
-Channels: 2
-Sample Rate: 44100
-Sample Size: 16
-Channels:    2
-Track 2:
+    OriginMP4Size=`ls -l ${Mp4File} | awk '{print $5}'`
 
+    TranscodeMP4Size=`ls -l ${OutputFile} | awk '{print $5}'`
 
-type:         Video
-duration: 13866 ms
-language: und
-media:
-sample count: 416
-timescale:    600
-duration:     8320 (media timescale units)
-duration:     13867 (ms)
-bitrate (computed): 1991.797 Kbps
-display width:  540.000000
-display height: 960.000000
-frame rate (computed): 30.000
-Sample Description 0
-Coding:      avc1 (H.264)
-Width:       540
-Height:      960
-Depth:       24
-AVC Profile:          100 (High)
-AVC Profile Compat:   0
-AVC Level:            31
-AVC NALU Length Size: 4
-AVC SPS: [2764001fac56c0881e7bd0]
-AVC PPS: [28ee3cb0]
-Codecs String: avc1.64001F
+    OriginMP4Size=`echo  "scale=2; ${OriginMP4Size} / 1024 / 1024"        | bc`
+    TranscodeMP4Size=`echo  "scale=2; ${TranscodeMP4Size} /1024 / 1024"   | bc`
 
+    DeltaSize=`echo  "scale=2; ${OriginMP4Size} - ${TranscodeMP4Size}"    | bc`
+    CompressRatio=`echo  "scale=2; ${DeltaSize} / ${OriginMP4Size} * 100" | bc`
+
+    TranscodeStatic="${MP4FileName}, ${OriginMP4Size}, ${TranscodeMP4Size}, ${CompressRatio}"
+
+    echo "${TranscodeStatic}" >>${TranscodeSummaryInfo}
 }
 
-runInitMP4Info()
+
+runGetAllMP4StaticInfo()
 {
-    let "VideoInfoLineNum = 24"
-    let "AudioInfoLineNum = 10"
-
-    let "VideoSampleCount = 0"
-    let "AudioSampleCount = 0"
-
-    let "VideoBitRate     = 0"
-    let "AudioBitRate     = 0"
-
-    let "VideoDuration    = 0"
-    let "AudioDuration    = 0"
-
-    let "VideoSize        = 0"
-    let "AudioSize        = 0"
-    let "VideoRatio       = 0"
-    let "AudioRatio       = 0"
-    let "MP4Size          = 0"
-
-    let "VideoWidth       = 0"
-    let "VideoHeight      = 0"
-    let "VideoFPS         = 0"
-    let "VideoCompreRate  = 0"
-
-    Profile=""
-    Level=""
-
-    VideoTrackInfo="Mp4Info_Video.txt"
-    AudioTrackInfo="Mp4Info_Audio.txt"
+    Command="${MP4ParserScript} ${InputDir} ${AllMP4Info}"
+    echo "Parse command is $Command"
+    ${Command}
 }
-
-runParseVideoInfo()
-{
-    VideoSampleCount=`cat ${VideoTrackInfo} | grep "sample count"  | awk 'BEGIN {FS=" "} {print $2}'`
-    #kbps
-    VideoBitRate=`cat ${VideoTrackInfo}     | grep "bitrate"       | awk 'BEGIN {FS=" "} {print $3}'`
-
-    VideoFPS=`cat ${VideoTrackInfo}         | grep "frame rate"    | awk 'BEGIN {FS=" "} {print $4}'`
-    VideoWidth=`cat ${VideoTrackInfo}       | grep "Width"         | awk 'BEGIN {FS=" "} {print $2}'`
-    VideoHeight=`cat ${VideoTrackInfo}      | grep "Height"        | awk 'BEGIN {FS=" "} {print $2}'`
-
-    Profile=`cat ${VideoTrackInfo} | grep "AVC Profile:" | awk 'BEGIN {FS="("} {print $2}' | awk 'BEGIN {FS=")"} {print $1}'`
-    Level=`cat ${VideoTrackInfo} | grep "AVC Level:"   | awk 'BEGIN {FS=" "} {print $2}'`
-}
-
-runParseAudioInfo()
-{
-    AudioSampleCount=`cat ${AudioTrackInfo} | grep "sample count" | awk 'BEGIN {FS=" "} {print $2}'`
-    #kbps
-    AudioBitRate=`cat ${AudioTrackInfo}     | grep "bitrate"      | awk 'BEGIN {FS=" "} {print $2}'`
-    AudioDuration=`cat ${AudioTrackInfo}    | grep "duration"     | awk 'BEGIN {FS=" "} {print $2}'`
-}
-
-runCalculateInfo()
-{
-    #bytes
-    MP4Size=`ls -l ${file} | awk '{print $5}'`
-
-    FrameCompressedRatio=`echo  "scale=2; ${FrameCompressedRatio} / ${DataNum}" | bc`
-
-
-
-}
-
-runParseOriginFile()
-{
-
-
-egrep -i -A${VideoInfoLineNum} "type: *Video" mp4info.txt >${VideoTrackInfo}
-egrep -i -A${AudioInfoLineNum} "type: *Audio" mp4info.txt >${AudioTrackInfo}
-
-
-}
-
-
 
 runTranscode()
 {
-
-    for file in ${InputDir}/*.mp4
+    for Mp4File in ${InputDir}/*.mp4
     do
-        OriginFlag=`echo "$file" | grep ${TranscodePattern}`
+        OriginFlag=`echo "$Mp4File" | grep ${TranscodePattern}`
         [ -z "${OriginFlag}" ] || continue
 
-        OutputFile="${file}_${TranscodePattern}_${Pattern}.mp4"
-        TransCommand="ffmpeg -i $file -c:a copy -c:v libx264 -profile:v high -level 3.1"
+        OutputFile="${Mp4File}_${TranscodePattern}_${Pattern}.mp4"
+        TransCommand="ffmpeg -i $Mp4File -c:a copy -c:v libx264 -profile:v high -level 3.1"
         TransCommand="$TransCommand -crf 24 ${x264Params} -y $OutputFile"
-        echo "****************************************"
-        echo "  file is $file"
+
+        echo -e "\033[32m ***************************************** \033[0m"
+        echo "  Mp4File is $Mp4File"
         echo "  TransCommand is : $TransCommand"
         echo "  addition enc param is: ${x264Params}"
-        echo "****************************************"
+        echo -e "\033[32m ***************************************** \033[0m"
 
         ${TransCommand}
 
+        runUpdateTranscodeStatic
     done
-
-
 }
 
+runPrompt()
+{
+    echo -e "\033[32m ************************************************************ \033[0m"
+    echo "  Transcode summary report, refer to:                                    "
+    echo "        --${TranscodeSummaryInfo}                                        "
+    echo "  All mp4 static info, refer to:                                         "
+    echo "        --${AllMP4Info}                                                  "
+    echo -e "\033[32m ************************************************************ \033[0m"
+}
+
+runCheck()
+{
+    let "Flag = 1"
+    [ -d ${Input} ] || let "Flag = 0"
+
+    if [ ${Flag} -eq 0 ]
+    then
+        echo "Input dir doest not exist, please double check"
+        runUsage
+        exit 1
+    fi
+
+}
 
 runMain()
 {
+    runCheck
 
-runInit
-runTranscode
+    runInit
 
+    runTranscode
+    runGetAllMP4StaticInfo >${AllMP4InfoParserConsole}
+    runPrompt
 
 }
+
+#*****************************************************
+
+if [ $# -lt 1 ]
+then
+    runUsage
+    exit 1
+fi
 
 InputDir=$1
 x264Params=$2
 Pattern=$3
 
 runMain
+#*****************************************************
 
 
 
