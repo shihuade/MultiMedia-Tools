fa731f435159220fb43d64ab8eda2101e547a136
Edward Shi <shihuade@users.noreply.github.com>
2017-07-28 16:01:32 +0800

    update

--- x264/run_x264.sh
+++ x264/run_x264.sh
@@ -9,7 +9,7 @@
 runUsage()
 {
     echo -e "\033[31m ***************************************** \033[0m"
-    echo " Usage:                                                    "
+    echo "  Usage:                                                   "
     echo "      $0  \$Input  \$option                                "
     echo "                                                           "
     echo "      $0  \$InputYUV:    YUV for x264 encoding             "
@@ -28,10 +28,15 @@ runInit()
     x264EncLog=""
     x264EncPerfInfo=""
 
-    AllMP4Info="${InputYUVDir}/Report_${FileNamePattern}_AllMP4Info_${EncParamName}.csv"
-    AllMP4InfoParserConsole="${InputYUVDir}/Report_${FileNamePattern}_AllMP4InfoDetail_${EncParamName}.txt"
+    ReportDir="${InputYUVDir}/Report"
+    BitStreamDir="${InputYUVDir}/BitStream"
+    mkdir -p ${ReportDir}
+    mkdir -p ${BitStreamDir}
 
-    x264EncReport="${InputYUVDir}/Report_${FileNamePattern}_Summary_${EncParamName}.csv"
+    AllMP4Info="${ReportDir}/Report_${FileNamePattern}_AllMP4Info_${EncParamName}.csv"
+    AllMP4InfoParserConsole="${ReportDir}/Report_${FileNamePattern}_AllMP4InfoDetail_${EncParamName}.txt"
+
+    x264EncReport="${ReportDir}/Report_${FileNamePattern}_Summary_${EncParamName}.csv"
 
     HeadLine="YUVFile, ParaName, ParamVal, CR, YUVSize(KBs), BitStreamSize(kBs)"
     HeadLine="${HeadLine},BitRate, PSNRY, PSNRU,  PSNRV, FPS, Time(s), SHA1-Org, SHA1-Trans"
@@ -71,11 +76,16 @@ runx264EncParam()
 {
     FPS=`echo $InputYUV | awk 'BEGIN {FS="fps"} {print $1}' | awk 'BEGIN {FS="_"} {print $NF}'`
     [ -z "$FPS" ] && FPS="30"
-    for EncParam in ${aEncParam[@]}
+
+    ParamNum=${#aEncParam[@]}
+    for((i=0; i<$ParamNum; i++))
     do
-        OutputBitStream="${InputYUV}_${FileNamePattern}_${EncParamName}_${EncParam}.264"
-        OutputMp4="${InputYUV}_${FileNamePattern}_${EncParamName}_${EncParam}.264.mp4"
-        x264EncLog="${InputYUV}_${FileNamePattern}_${EncParamName}_${EncParam}_enc.txt"
+        EncParam="${aEncParam[$i]}"
+        EncParamString=`echo ${EncParam} | awk '{for(i=1; i<=NF; i++) printf("%s_", $i) }'`
+        YUVName=`basename $InputYUV`
+        OutputBitStream="${BitStreamDir}/${YUVName}_${FileNamePattern}_${EncParamName}_${EncParamString}.264"
+        OutputMp4="${InputYUV}_${FileNamePattern}_${EncParamName}_${EncParamString}.264.mp4"
+        x264EncLog="${BitStreamDir}/${YUVName}_${FileNamePattern}_${EncParamName}_${EncParamString}_enc.txt"
         EncCommand="x264 --psnr --fps ${FPS} ${EncParamPlus}"
         EncCommand="${EncCommand} ${EncParamArg} ${EncParam}  -o ${OutputBitStream} ${InputYUV}"
         MP4Command="ffmpeg -framerate ${FPS} -i ${OutputBitStream} -c copy -y ${OutputMp4}"
@@ -167,10 +177,10 @@ runx264EncInitProfile()
 runx264EncInitLevel()
 {
     EncParamName="Level"
-    EncParamPlus="--profile high"
+    EncParamPlus="--profile high --crf 23"
     EncParamArg="--level "
 
-    aEncParam=(20 30 40  50 52 )
+    aEncParam=(20 21 22 30 31 32  40 )
 }
 
 runx264EncMe()
@@ -182,23 +192,120 @@ runx264EncMe()
     aEncParam=(dia hex umh  esa tesa )
 }
 
-runx264EncIFrame()
-{
-    EncParamName="I_NR_SceneCut"
-    EncParamPlus="--profile high --level 31 -crf --nr 600"
-    EncParamArg="--scenecut"
-
-    aEncParam=(10 20 30 40 50 60 70)
-}
 
 runx264EncNoise()
 {
-    EncParamName="NR_0_600"
-    EncParamPlus="--profile high --level 31 --crf 23"
+    EncParamName="NR_0_600_crf_26"
+    EncParamPlus="--profile high --level 31 --crf 26"
     EncParamArg="--nr"
 
     aEncParam=(0 600  1000)
 }
+
+runx264EncIFrame()
+{
+    EncParamName="I_NoNR_SceneCut_0125"
+    EncParamPlus="--profile high --level 31 --crf 23"
+    EncParamArg="--scenecut"
+
+    #scenecut I frames num increase when value increase, and the CR decrease
+    #also change B frame structure
+    aEncParam=(400 800)
+}
+
+runBStructure()
+{
+    #-b, --bframes <integer>     Number of B-frames between I and P [3]
+    #--b-adapt <integer>     Adaptive B-frame decision method [1]
+        #Higher values may lower threading efficiency.
+        #- 0: Disabled
+        #- 1: Fast
+        #- 2: Optimal (slow with high --bframes)
+    #--b-bias <integer>      Influences how often B-frames are used [0]
+    #--b-pyramid <string>    Keep some B-frames as references [normal]
+        #- none: Disabled
+        #- strict: Strictly hierarchical pyramid
+        #- normal: Non-strict (not Blu-ray compatible)
+    #--open-gop              Use recovery points to close GOPs
+    #    Only available with b-frames
+
+    EncParamName="BStructure_OpenGOP_NR"
+    EncParamPlus="--profile high --level 31 --crf 23 --bframes 3 "
+    EncParamArg=""
+
+    #--b-adapt =2 will be very slow and CR not sure to enhance
+    #--bframes 在渐变场景，B帧并不是也多越好，越多，B帧的压缩率会大幅降低
+    #          在快速切换的场景，B帧多略优
+    #--b-pyramid  =1 或者2 时，参考帧B帧的图像质量较差，参考压缩效率降低，整体压缩性能不一定提升
+    #--b-bias -90～100， 越大，B帧倾向性越大，但是，B帧越大，B帧的压缩比会降低，总体压缩性能不一定提升，甚至降低
+    #--open-gop 测试结果显示，压缩比没有变化，但是编码时间会增加
+    aEncParam[0]="--nr 600"
+    aEncParam[1]="--open-gop --nr 600"
+}
+
+
+runx264EncRCMode()
+{
+    EncParamName="RC_AQTest_CRF23"
+    EncParamPlus="--profile high --level 31 --crf 23"
+    EncParamArg="--aq-mode "
+
+    #CQP 模式，文件大小不恒定
+    #CBR 模式，码率控制较好，但是视频质量变化大
+    #MaxQP， 目前36和40相同，但当前参数设置基本和36相似，质量相似
+    #--qcomp  默认0.6， 越小，mbtree权重越大，压缩比越高， 建议选择0.5～0.6之间的
+    aEncParam=( 0 1 2 3  )
+}
+
+runx264EncSkip()
+{
+    EncParamName="EarlySkip_CRF23"
+    EncParamPlus="--profile high --level 31"
+    EncParamArg=" "
+
+    #默认打开，P_SKIP MV 快速决策，测试集显示，对编码效率有提升
+    aEncParam[0]=" --crf 23 --no-fast-pskip"
+    aEncParam[1]=" --crf 23"
+
+}
+
+runx264EncWeight()
+{
+    EncParamName="WeightP_CRF23"
+    EncParamPlus="--profile high --level 31"
+    EncParamArg="--weightp "
+
+    #--no-weightb 默认打开B帧加权，对编码效率有提升，但不明显，时间相近
+    #--weightbp  默认2, P帧加权，对编码效率有提升，时间增加10%左右
+    #aEncParam[0]=" --crf 23 "
+    aEncParam=(0 1 2)
+}
+
+runx264EncRef()
+{
+    EncParamName="Ref_CRF23"
+    EncParamPlus="--profile high --level 31 --crf 23"
+    EncParamArg="--ref "
+
+    aEncParam=(2 3 4 5)
+}
+
+runx264EncDeblocking()
+{
+    EncParamName="Deblock_CRF23"
+    EncParamPlus="--profile high --level 31 --crf 23"
+    EncParamArg="--deblock "
+
+    aDeblocingParams=(-6 -3  -1 0 1 2 3 6)
+    let "i=0"
+    for Deblock in ${aDeblocingParams[@]}
+    do
+        aEncParam[$i]=" ${Deblock}:${Deblock} "
+        let "i ++"
+    done
+    #aEncParam=(2 3 4 5)
+}
+
 
 runInitForTestParams()
 {
@@ -207,8 +314,17 @@ runInitForTestParams()
     #runx264EncInitProfile
     #runx264EncInitLevel
     #runx264EncMe
-    #runx264EncIFrame
-    runx264EncNoise
+#runx264EncNoise
+#runx264EncIFrame
+#runBStructure
+#runx264EncRCMode
+#runx264EncSkip
+
+#runx264EncWeight
+#runx264EncInitLevel
+#runx264EncRef
+
+runx264EncDeblocking
 
     runInit
 }
