e60af7853411f7a103d7a796554e53c758745cbf
Edward Shi <shihuade@users.noreply.github.com>
2017-09-25 11:05:37 +0800

    update dowload mp4 script

--- FFMPEG/run_DownLoadMP4Files.sh
+++ FFMPEG/run_DownLoadMP4Files.sh
@@ -8,34 +8,38 @@ runInit()
     MP4FileList="MP4FileIPList.txt"
     OutputDir="${CurrentDir}/MP4FileList"
 
-    mkdir -p ${OutputDir}c
+    mkdir -p ${OutputDir}
 }
 
 runPrompt()
 {
     echo -e "\033[32m ***************************************** \033[0m"
     echo -e "\033[32m  FileIndex:   ${FileIndex}                \033[0m"
+    echo -e "\033[32m  FailedIndex: ${FailedIndex}              \033[0m"
     echo -e "\033[32m  MP4FileName: ${MP4FileName}              \033[0m"
     echo -e "\033[32m  OutputFile:  ${OutputFile}               \033[0m"
+    echo -e "\033[32m  MP4FileIP:   ${MP4FileIP}                \033[0m"
+    echo -e "\033[32m  Command:     ${Command}                  \033[0m"
     echo -e "\033[32m ***************************************** \033[0m"
 }
 
 runDownload()
 {
-    let "FileIndex = 0"
+    let "FileIndex   = 0"
+    let "FailedIndex = 0"
     while read line
     do
-        MP4File=${line}
-        MP4FileName=`echo ${MP4File} | awk 'BEGIN {FS="/"} {print $NF}'`
+        MP4FileIP=${line}
+        MP4FileName=`echo ${MP4FileIP} | awk 'BEGIN {FS="/"} {print $NF}'`
         OutputFile="${OutputDir}/${MP4FileName}"
         MP4Flag=`echo ${MP4FileName} | grep ".mp4"`
+        Command="wget ${MP4FileIP}  -O ${OutputFile}"
+
         [ -z "${MP4Flag}" ] && continue
 
-
-        wget ${MP4File}  -O ${OutputFile}
-        [ $? -ne 0 ] && continue
-
         runPrompt
+        ${Command}
+        [ $? -ne 0 ] && echo -e "\033[31m Download failed! \033[0m" && let "FailedIndex +=1" &&continue
 
         let "FileIndex += 1"
 
