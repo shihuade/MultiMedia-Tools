c28843566efb6639cca00d737b7f7eaae98b5f7e
Edward Shi <shihuade@users.noreply.github.com>
2017-09-26 10:24:53 +0800

    add script for parsing one image from mp4 file

---
+++ ImageView/run_ParseOneImageFromMP4.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#********************************************************************************
+#  parse one image from mp4
+#
+#     ViewModule/index.html will browse image from
+#     ViewModule/images
+#
+#********************************************************************************
+
+runUsage()
+{
+    echo -e "\033[31m ***************************************************** \033[0m"
+    echo "   Usage:                                                        "
+    echo "      $0  \$InputMp4 \${OutputImage} \${TimeStamp}               "
+    echo -e "\033[31m ***************************************************** \033[0m"
+}
+
+runPrompt()
+{
+    echo -e "\033[32m ****************************************\033[0m"
+    echo -e "\033[33m TimeStamp      is: ${TimeStamp}         \033[0m"
+    echo -e "\033[32m OutputImage    is: ${OutputImage}       \033[0m"
+    echo -e "\033[34m Command        is: ${Command}           \033[0m"
+    echo -e "\033[32m *************************************** \033[0m"
+}
+
+runParseOneComparisonPictureFromMP4()
+{
+    #file from MP4File01
+    Format="image2"
+    TempFFMPEGImageLog="Log_FFMPEG_ImageParse.txt"
+    Command="ffmpeg -ss ${TimeStamp} -i ${InputMp4} -an  -vframes 1 -f ${Format} -y ${OutputImage}"
+
+    runPrompt
+
+$Command  #2>>${TempFFMPEGImageLog}
+    [ $? -ne 0 ] && return 1
+}
+
+runCheck()
+{
+    if [ ! -e ${InputMp4} ]
+    then
+        echo "mp4 file not exist, please double check"
+        runUsage
+        exit 1
+    fi
+}
+
+runMain()
+{
+    runCheck
+
+    runParseOneComparisonPictureFromMP4
+    return $?
+}
+
+#*************************************************************************************
+if [ $# -lt 3 ]
+then
+    runUsage
+    exit 1
+fi
+
+InputMp4=$1
+OutputImage=$2
+TimeStamp=$3
+
+runMain
+#*************************************************************************************
+
