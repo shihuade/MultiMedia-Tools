3ceb3feaf3d985a4d01c3886aaf617440349abbd
Edward Shi <shihuade@users.noreply.github.com>
2017-09-29 15:44:22 +0800

    update

--- FFMPEG/run_CPUPerformanceTest.sh
+++ FFMPEG/run_CPUPerformanceTest.sh
@@ -4,10 +4,10 @@
 runInit()
 {
     CodecParam=" -c:a copy -c:v libx264 -profile:v high -level 3.1 "
-    x264Arg01=" -x264opts scenecut=30:subme=0:trellis=0 -bf 2 -refs 2 "
-    x264Arg02=" -x264opts scenecut=30:subme=0:trellis=0:threads=1 -bf 2 -refs 2 "
+    x264Arg01=" -x264opts scenecut=30:subme=0:trellis=0:threads=4 -bf 2 -refs 2 "
+    x264Arg02=" -x264opts scenecut=30:subme=0:trellis=0:threads=3 -bf 2 -refs 2 "
     x264Param=" -rc-lookahead 10 -crf 22 -qcomp 0.54 -deblock 0 -nr 450"
-    PluseParam=" -movflags faststart -use_editlist 0 -y"
+    PluseParam="-threads 4 -movflags faststart -use_editlist 0 -y"
 
     InputMP4="/Users/huade/Desktop/CopyVideo/Import-Copy-04.mp4"
     OutputMP4="/Users/huade/Desktop/CopyVideo/Import-Copy-04.mp4_orgdesign.mp4"
@@ -15,8 +15,8 @@ runInit()
     Command1="ffmpeg -i ${InputMP4} ${CodecParam} ${x264Arg01} ${x264Param} ${PluseParam} ${OutputMP4}"
     Command2="ffmpeg -i ${InputMP4} ${CodecParam} ${x264Arg02} ${x264Param} ${PluseParam} ${OutputMP4}"
 
-    let "LoopNum   = 20"
-    let "SleepTime = 12"
+    let "LoopNum   = 30"
+    let "SleepTime = 20"
 }
 
 runPrompt()
@@ -35,7 +35,7 @@ runCPUTest()
         echo -e "\033[33m Label is   : ${Label}                     \033[0m"
         echo -e "\033[33m Command is : ${Command}                   \033[0m"
         echo -e "\033[33m ***************************************** \033[0m"
-        ${Command}
+        ${Command} 2>Log_CPUTest.txt
 
     done
 }
