7a59146ed12d51f7d6aaf6f8c5b1e1565528b31a
Edward Shi <shihuade@users.noreply.github.com>
2017-10-23 10:18:27 +0800

    update Basic 02

---
+++ CMake/CMakeBasic/Baisc-02/hello.c
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+void sayhello()
+{
+
+    printf("hello, my cmake ! /n");
+
+}
---
+++ CMake/CMakeBasic/Baisc-02/main.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+
+int main()
+{
+
+    sayhello();
+    sayhello();
+
+    return 0;
+}
