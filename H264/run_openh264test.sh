8cee1746ba0faad99993a5b5b67cc138636ca702
Edward Shi <shihuade@users.noreply.github.com>
2017-11-16 21:42:51 +0800

    update param

--- H264/run_openh264test.sh
+++ H264/run_openh264test.sh
@@ -12,12 +12,24 @@ PicH=848
 
 runTestAll()
 {
-Command="openh264enc -org ${YUVFile} -sw ${PicW} -sh ${PicH} -fs 0  -numl 1  -numtl 1 -frin 25 -cabac 1  -dw 0 ${PicW} -dh 0 ${PicH} -frout 0 25 -bf ${BitStream}"
+
+BitStream="Test_QP26-10-3-30I_high-rc.264"
+#Command="openh264enc -org ${YUVFile} -iper 30 -sw ${PicW} -sh ${PicH} -fs 0  -numl 1  -numtl 1 -frin 25 -cabac 1  -db 3  -denois 1   -scene 1  -bgd 1  -aq 1 -dw 0 ${PicW} -dh 0 ${PicH} -lqp 0 20 -ltarb 0 1000  -dprofile 0 100 -frout 0 25 -bf ${BitStream}"
+
+#Command="openh264enc -org ${YUVFile} -iper -1 -sw ${PicW} -sh ${PicH} -fs 0  -numl 1  -numtl 1 -frin 25 -cabac 1  -denois 1  -scene 1  -bgd 1  -aq 1 -dw 0 ${PicW} -dh 0 ${PicH}  -dprofile 0 100 -frout 0 25 -maxqp 28 -minqp 10 -rc 1 -deblockIdc 0 -complexity 2 -bf ${BitStream}"
+Command="openh264enc -org ${YUVFile} -iper 30 -sw ${PicW} -sh ${PicH} -fs 0  -numl 1  -numtl 1 -frin 25 -cabac 1  -denois 1  -scene 1  -bgd 1  -aq 1 -dw 0 ${PicW} -dh 0 ${PicH}  -dprofile 0 100 -frout 0 25 -maxqp 26 -minqp 10 -rc 1 -ltarb 0 1000 -deblockIdc 0 -complexity 2 -bf ${BitStream}"
+
     echo "*************************************"
     echo " Command is ${Command}"
     echo "*************************************"
 
 ${Command}
+
+echo "*************************************"
+echo " FFMPEG mexuer"
+echo "*************************************"
+ffmpeg -framerate 25 -i ${BitStream} -c copy -y ${BitStream}.mp4
+
 }
 
 
