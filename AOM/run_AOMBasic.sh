0442cf8ffb5ebeeec9beac7607cc877ff250a087
shihuade <shihuadesysu@gmail.com>
2018-02-24 11:12:14 +0800

    update aom

---
+++ AOM/run_AOMBasic.sh
@@ -0,0 +1,8 @@
+
+
+
+cmake .. -DENABLE_CCACHE=1 -DAOM_TARGET_CPU=generic -DENABLE_DOCS=0 -DCONFIG_ACCOUNTING=1 -DCONFIG_INSPECTION=1 -DCONFIG_MULTITHREAD=0 -DCONFIG_RUNTIME_CPU_DETECT=0 -DCONFIG_UNIT_TESTS=0 -DCONFIG_WEBM_IO=0 -DCMAKE_TOOLCHAIN_FILE=../../emsdk-portable/emscripten/1.37.33/cmake/Modules/Platform/Emscripten.cmake
+
+index.html?decoder=inspect.js&file=a.ivf&decoder=bDec.js&file=b.ivf
+
+index.html?decoder=../aom/javascript_build/examples/inspect.js&file=../aom/javascript_build/examples/Test.ivf 
